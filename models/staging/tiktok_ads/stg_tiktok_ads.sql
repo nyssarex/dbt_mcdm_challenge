e0a0e790d017ec04420212195a75eedb72298dae
Amir Nyssambayev <nyssambayevamir@gmail.com>
2023-06-21 16:44:52 +0000

    intermediate error with ref

--- models/staging/tiktok_ads/stg_tiktok_ads.sql
+++ models/staging/tiktok_ads/stg_tiktok_ads.sql
@@ -1,8 +1,20 @@
 with source as 
 (
     select 
-    *
-    
+    date
+    ,ad_id
+    ,ad_status
+    ,ad_text
+    ,channel
+    ,impressions 
+    ,clicks
+    ,rt_installs
+    ,video_views
+    ,purchase
+    ,registrations
+    ,add_to_cart
+    ,conversions
+    ,spend
      from {{ source('bigquery','src_ads_tiktok_ads_all_data') }}
 )
 
--- models/staging/twitter_ads/stg_twitter_ads.sql
+++ models/staging/twitter_ads/stg_twitter_ads.sql
@@ -1,7 +1,20 @@
 with source as 
 (
     select 
-    *
+    date
+    ,campaign_id
+    ,channel
+    ,url
+    ,text
+    ,impressions
+    ,engagements
+    ,clicks
+    ,comments
+    ,likes
+    ,url_clicks
+    ,retweets
+    ,video_total_views
+    ,spend
     
      from {{ source('bigquery','src_promoted_tweets_twitter_all_data') }}
 )
